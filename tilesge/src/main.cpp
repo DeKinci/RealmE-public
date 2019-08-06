b8dfb5fa691dd321336b138c4cee51eb32335381
DeKinci <knd2000.2@gmail.com>
2019-08-06 06:58:23 +0300

    better main

--- tilesge/src/main.cpp
+++ tilesge/src/main.cpp
@@ -18,26 +18,6 @@ static const struct {
         {0.f,   0.6f,  0.f, 0.f, 1.f}
 };
 
-static const char *vertex_shader_text =
-        "#version 110\n"
-        "uniform mat4 MVP;\n"
-        "attribute vec3 vCol;\n"
-        "attribute vec2 vPos;\n"
-        "varying vec3 color;\n"
-        "void main()\n"
-        "{\n"
-        "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-        "    color = vCol;\n"
-        "}\n";
-
-static const char *fragment_shader_text =
-        "#version 110\n"
-        "varying vec3 color;\n"
-        "void main()\n"
-        "{\n"
-        "    gl_FragColor = vec4(color, 1.0);\n"
-        "}\n";
-
 static void error_callback(int error, const char *description) {
     fprintf(stderr, "Error: %s\n", description);
 }
@@ -47,13 +27,7 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
-static const GLfloat g_vertex_buffer_data[] = {
-        -1.0f, -1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-        0.0f,  1.0f, 0.0f,
-};
-
-int main() {
+GLFWwindow * init() {
     glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         exit(EXIT_FAILURE);
@@ -77,6 +51,11 @@ int main() {
         exit(EXIT_FAILURE);
     }
     glfwSwapInterval(1);
+    return window;
+}
+
+int main() {
+    GLFWwindow *window = init();
 
     GLuint VertexArrayID;
     glGenVertexArrays(1, &VertexArrayID);
